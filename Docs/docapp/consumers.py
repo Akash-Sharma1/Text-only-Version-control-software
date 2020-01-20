950668a151c44b5ae66935220b09f445fd43fff5
Akash Sharma <aakashsharmajc100@gmail.com>
2020-01-20 19:10:31 +0530

    parial woking

--- Docs/docapp/consumers.py
+++ Docs/docapp/consumers.py
@@ -5,7 +5,6 @@ import json
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.c_name = self.scope['url_route']['kwargs']['c_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
         # Join room group
@@ -26,24 +25,42 @@ class ChatConsumer(AsyncWebsocketConsumer):
     # Receive message from WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        
+        #print("text")
+        #print(text_data)
+        #print("dictionary")
+        #print(text_data_json)
+        op = text_data_json['op']
+        if op=="":
+            return
+        position = text_data_json['pos']
+        time = text_data_json['time']
+        name = text_data_json['name']
+        word = text_data_json['word']
         # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message,
-                'name' : self.c_name
+                'op': op,
+                'pos' :position,
+                'time': time,
+                'word': word,
+                'name' : name
             }
         )
             
     # Receive message from room group
     async def chat_message(self, event):
-        message = event['message']
+        op = event['op']
+        pos = event['pos']
+        time = event['time']
+        word = event['word']
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
-            'message': message,
-            'name' : event['name']
+                'op': op,
+                'pos' : pos,
+                'time': time,
+                'word': word,
+                'name' : event['name']
         }))
